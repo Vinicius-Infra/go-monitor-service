eb3ac0ff25a8692885e291d29fea5b9a0c9fb3ec
ViniciusTechnology <devopsengineer048@gmail.com>
2026-04-11 09:58:38 -0300

    feat: implement database ping for Java API monitoring

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
-package main 
+package main
 
-import "fmt" 
+import (
+	"database/sql" // Ferramenta para falar com bancos SQL
+	"fmt"          // Ferramenta para escrever mensagens
+	"log"          // Ferramenta para registrar erros
+	_ "github.com/lib/pq" // O "driver" do Postgres (o _ significa que usamos ele quietinho no fundo)
+)
 
-func main() { 
-    fmt.Println("Olá, Vinicius! Seu primeiro código em Go está vivo! 🚀")
+func main() {
+	// 1. A nossa "frase mágica" de conexão (Igual a que usamos no application.properties)
+	// host, port, user, password, dbname e sslmode
+	connStr := "host=localhost port=5433 user=postgres password=postgres dbname=microservicesdb sslmode=disable"
+
+	// 2. O Go tenta abrir a porta do banco
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Erro ao configurar o banco: ", err)
+	}
+	defer db.Close() // Isso garante que a porta feche quando terminarmos (boa educação!)
+
+	// 3. O PING: É aqui que o Vigia pergunta: "Tem alguém aí?"
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("❌ A API Java está fora do ar ou o banco está desligado!")
+		log.Fatal(err)
+	}
+
+	fmt.Println("✅ Sucesso! O monitor Go conseguiu falar com o banco da API Java na porta 5433.")
 }
